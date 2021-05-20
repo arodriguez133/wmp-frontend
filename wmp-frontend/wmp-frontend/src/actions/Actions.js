7fe6b02b760b5916f4ede405e1b6be05807865f8
aurelions <you@example.com>
2021-05-20 13:08:15 -0500

    scaffolding + action

---
+++ wmp-frontend/wmp-frontend/src/actions/Actions.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+//THUNKY FUN
+
+export const ADD_PLANT = 'ADD_PLANT'
+export const SET_ERROR = 'SET_ERROR'
+export const FETCH_PLANTS_START = 'FETCH_PLANTS_START'
+export const FETCH_PLANTS_SUCCESS = 'FETCH_PLANTS_SUCCESS'
+export const FETCH_PLANTS_FAILURE = 'FETCH_PLANTS_FAILURE'
+
+
+//...add a new plant into our object
+export const addPlant = (nickname, species, water, description) => {
+
+    const newPlant = {
+        nickname: nickname,
+        species: species,
+        water: water,
+        description: description,
+    }
+
+    return ({ type: ADD_PLANT, payload: newPlant });
+    
+}
+
+// Error Message
+export const setErrorMsg = (errorMsg) => {
+    return ({ type: SET_ERROR, payload: errorMsg })
+}
+
+//API fetch function
+export const fetchPlants = () => (dispatch) => {
+    dispatch ({ type: FETCH_PLANTS_START })
+
+    //fetch the api here
+    axios.get(`https://water-my-plants-build.herokuapp.com/plants`)
+        .then(res => {
+            dispatch({ type: FETCH_PLANTS_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            console.log("error: ", err)
+            dispatch({ type: FETCH_PLANTS_FAILURE, payload: err})
+        })
+}
