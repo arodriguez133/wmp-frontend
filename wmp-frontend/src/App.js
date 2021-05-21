63e8c6b89202527cbcbd705f1a39ee3626d17989
Trevor Mandina <trevorm157@hotmail.com>
2021-05-21 18:42:16 -0500

    Login feature and routes setup

--- wmp-frontend/src/App.js
+++ wmp-frontend/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+
+import Login from './components/Login'
+import PrivateRoute from './components/PrivateRoute'
+
+//WMP = watermyplants.js
+import WMP from './components/WaterMyPlants'
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
-      
+      <Route exact path='/' component={Login} />
+      <PrivateRoute exact path="/watermyplants" component={WMP} />
     </div>
+
+    </Router>
   );
 }
 
